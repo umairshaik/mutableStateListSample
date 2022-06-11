fa8d0b3f980a0f854fca78904c37b594d99efa41
umair.shaik <u.shk.anjum@gmail.com>
2022-06-12 01:21:27 +0530

    solution-1

--- app/src/main/java/com/example/quickflowtry/MainActivityViewModel.kt
+++ app/src/main/java/com/example/quickflowtry/MainActivityViewModel.kt
@@ -1,12 +1,13 @@
 package com.example.quickflowtry
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 class MainActivityViewModel : ViewModel() {
     private var iterator: Int = 1
-    private val flowList: MutableList<Int> = mutableListOf()
+    private val flowList: MutableList<Int> = mutableStateListOf()
     private val _uiState = MutableStateFlow<List<Int>>(emptyList())
 
     val uiState: StateFlow<List<Int>>
