2f69c2e135a0449b42007075f7a90959008439d3
umair.shaik <u.shk.anjum@gmail.com>
2022-06-12 01:13:40 +0530

    Solution

--- app/src/main/java/com/example/quickflowtry/MainActivity.kt
+++ app/src/main/java/com/example/quickflowtry/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -23,11 +22,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,10 +37,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             QuickFlowTryTheme {
-
-                //val data by mainActivityViewModel.uiState.collectAsState()
                 val scaffoldState = rememberScaffoldState()
 
                 Scaffold(scaffoldState = scaffoldState) {
@@ -64,43 +58,59 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun ParentLayout(viewModel: MainActivityViewModel = viewModel()) {
-        val listDat: SnapshotStateList<List<Int>> = remember {
+        val snapshotStateList: SnapshotStateList<List<Int>> = remember {
             mutableStateListOf()
         }
-        val data by viewModel.uiState.collectAsState()
-        //val remember by remember { mutableStateListOf(listOf()) }
-        //var listOfNum by remember { mutableStateOf(data) }
-        val onClick = { viewModel.incrementByOne() }
-        listDat.add(data.data)
-
-        Column {
-            DataHolder(data = listDat.last(), onClick = onClick)
-            HelloScreen()
+        val uiStateModel by viewModel.uiState.collectAsState()
+        val onClickIncrement = { viewModel.incrementByOne() }
+        snapshotStateList.add(uiStateModel.data)
+        val modifier = Modifier
+        Column(
+            modifier = modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            DataHolder(
+                modifier = modifier,
+                data = snapshotStateList.last(),
+                onClick = onClickIncrement
+            )
         }
     }
 
     @Composable
-    private fun DataHolder(data: List<Int>, onClick: () -> Unit) {
-        //val remember: SnapshotStateList<List<Int>> = remember { mutableStateListOf(data) }
-        Column {
-            Button(onClick = onClick) { Text(text = "Add") }
-            Greeting(data)//remember.toList().last())
+    private fun DataHolder(modifier: Modifier = Modifier, data: List<Int>, onClick: () -> Unit) {
+        Column(modifier = modifier.fillMaxWidth()) {
+            Button(
+                modifier = modifier
+                    .align(alignment = Alignment.CenterHorizontally)
+                    .padding(10.dp),
+                onClick = onClick,
+            ) { Text(text = "Add One more Item") }
+            Greeting(modifier = modifier, data)
         }
     }
 }
 
 @Composable
-fun Greeting(name: List<Int>, listState: LazyListState = rememberLazyListState()) {
-    // val listState: LazyListState = rememberLazyListState()
-    // val remember by remember { mutableStateOf(name) }
+fun Greeting(
+    modifier: Modifier = Modifier,
+    name: List<Int>,
+    listState: LazyListState = rememberLazyListState()
+) {
     if (name.isEmpty()) {
-        Text(text = "You not have chats")
+        Text(
+            text = "You not have numbers added",
+            modifier = modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
     } else {
         LazyColumn(state = listState) {
             items(name) {
                 Text(
-                    text = it.toString(),
-                    modifier = Modifier.fillMaxWidth(),
+                    text = "you have added $it",
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(5.dp),
                     textAlign = TextAlign.Center
                 )
             }
@@ -112,30 +122,6 @@ fun Greeting(name: List<Int>, listState: LazyListState = rememberLazyListState()
 @Composable
 fun DefaultPreview() {
     QuickFlowTryTheme {
-        Greeting(listOf(1, 2, 3, 4))
+        Greeting(modifier = Modifier, listOf(1, 2, 3, 4))
     }
 }
-
-@Composable
-fun HelloScreen() {
-    var name by rememberSaveable { mutableStateOf("") }
-
-    HelloContent(name = name, onNameChange = { name = it })
-}
-
-@Composable
-fun HelloContent(name: String, onNameChange: (String) -> Unit) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(
-            text = "Hello, $name",
-            modifier = Modifier.padding(bottom = 8.dp),
-            style = MaterialTheme.typography.h5
-        )
-        OutlinedTextField(
-            value = name,
-            onValueChange = onNameChange,
-            label = { Text("Name") }
-        )
-    }
-}
-
--- app/src/main/java/com/example/quickflowtry/MainActivityViewModel.kt
+++ app/src/main/java/com/example/quickflowtry/MainActivityViewModel.kt
@@ -3,13 +3,9 @@ package com.example.quickflowtry
 import android.os.Parcelable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.getAndUpdate
-import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -18,7 +14,6 @@ data class UiStateModel(val data: List<Int> = mutableStateListOf()) : Parcelable
 class MainActivityViewModel : ViewModel() {
     private var iterator: Int = 1
     private val flowList: MutableList<Int> = mutableListOf()
-    private val holder = MutableSharedFlow<List<Int>>()
     private val _uiState = MutableStateFlow(UiStateModel())
 
     val uiState: StateFlow<UiStateModel>
@@ -26,16 +21,6 @@ class MainActivityViewModel : ViewModel() {
 
     fun incrementByOne() {
         flowList.add(iterator++)
-        viewModelScope.launch {
-            holder.emit(flowList)
-        }
-    }
-
-    init {
-        viewModelScope.launch {
-            holder.collectLatest { data ->
-                _uiState.getAndUpdate { _uiState.value.copy(data = data) }
-            }
-        }
+        _uiState.getAndUpdate { _uiState.value.copy(data = flowList) }
     }
 }
